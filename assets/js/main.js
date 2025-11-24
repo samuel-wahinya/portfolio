78dcf37fa83076d3960e46b482ee1448c9f49c03
samuel-wahinya <gitausam45@outlook.com>
2025-11-24 17:34:32 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,4 @@
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
 
--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,1 +1,27 @@
+const titles = [
+    "Data Warehouse Engineer",
+    "SQL Data Analyst",
+    "Tableau Dashboard Developer"
+];
+
+let i = 0, j = 0, deleting = false;
+const speed = 80;
+const typeSpan = document.getElementById("typewriter");
+
+function typeEffect() {
+    let current = titles[i];
+
+    if (!deleting) {
+        typeSpan.textContent = current.slice(0, j++);
+        if (j > current.length + 10) deleting = true;
+    } else {
+        typeSpan.textContent = current.slice(0, j--);
+        if (j < 0) {
+            deleting = false;
+            i = (i + 1) % titles.length;
+        }
+    }
+    setTimeout(typeEffect, speed);
+}
+typeEffect();
 
